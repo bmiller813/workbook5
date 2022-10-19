fb405ba93198cbd664973e53cb57b92a2ae84703
Bryan <bmiller@tb.yearup.org>
2022-10-19 11:11:34 -0400

    Finished (BONUS) EXERCISE 3 / SLIDE - 32 / es6_vehicle_search.js

---
+++ WB5-exercises/ES6Scripts/es6_vehicle_search.js
@@ -0,0 +1,81 @@
+//EXERCISE 3 (BONUS) / SLIDE - 32 / PAGE 1-28 - 30
+
+let vehicles = [
+    {
+        color: "Silver",
+        type: "Minivan",
+        registrationState: "CA",
+        licenseNo: "ABC-101",
+        registrationExpires: new Date("3-10-2022"),
+        capacity: 7
+    },
+    {
+        color: "Red",
+        type: "Pickup Truck",
+        registrationState: "TX",
+        licenseNo: "A1D-2NC",
+        registrationExpires: new Date("8-31-2023"),
+        capacity: 3
+    },
+    {
+        color: "White",
+        type: "Pickup Truck",
+        registrationState: "TX",
+        licenseNo: "A22-X00",
+        registrationExpires: new Date("9-31-2023"),
+        capacity: 6
+    },
+    {
+        color: "Red",
+        type: "Car",
+        registrationState: "CA",
+        licenseNo: "ABC-222",
+        registrationExpires: new Date("12-10-2022"),
+        capacity: 5
+    },
+    {
+        color: "Black",
+        type: "SUV",
+        registrationState: "CA",
+        licenseNo: "EEE-222",
+        registrationExpires: new Date("12-10-2021"),
+        capacity: 7
+    },
+    {
+        color: "Red",
+        type: "SUV",
+        registrationState: "TX",
+        licenseNo: "ZZ2-101",
+        registrationExpires: new Date("5-30-2022"),
+        capacity: 5
+    },
+    {
+        color: "White",
+        type: "Pickup Truck",
+        registrationState: "TX",
+        licenseNo: "CAC-7YT",
+        registrationExpires: new Date("1-31-2022"),
+        capacity: 5
+    },
+    {
+        color: "White",
+        type: "Pickup Truck",
+        registrationState: "CA",
+        licenseNo: "123-ABC",
+        registrationExpires: new Date("3-31-2023"),
+        capacity: 5
+    }
+];
+
+// Which vehicles are RED?
+//console.log(vehicles.find(r => r.color == "Red").licenseNo); Ask if there is a way to use find to get same result as filter (Im thinking use a function)
+vehicles.filter(r => r.color == "Red").forEach(r => console.log("Red Vehicles: " , r.licenseNo));
+
+// Which vehicles have registrations that are expired?
+vehicles.filter(e => e.registrationExpires < new Date()).forEach(r => console.log("Expired Registration: " , r.licenseNo));
+
+// Which vehicles that hold at least 6 people?
+vehicles.filter(h => h.capacity >= 6).forEach(r => console.log("Minimum Capacity: " , r.licenseNo));
+
+// Which vehicles have license plates that end with "222"?
+vehicles.filter(e => e.licenseNo.endsWith("222")).forEach(r => console.log("Ends in 222: " , r.licenseNo));
