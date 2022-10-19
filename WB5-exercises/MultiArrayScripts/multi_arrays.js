b654c4bfec88452459cf278b9eaffc6a0641bfe6
Bryan <bmiller@tb.yearup.org>
2022-10-19 15:38:16 -0400

    ImagesDemo, still needs a bit of work but the main idea is there

--- WB5-exercises/MultiArrayScripts/multi_arrays.js
+++ WB5-exercises/MultiArrayScripts/multi_arrays.js
@@ -12,3 +12,21 @@ console.log(lockerAndAccessCode[1]);
 //index out of range as ther is no third value on the inner lists
 // yields undefined
 console.log(lockerAndAccessCode[2][2]);
+
+//SLIDE 37
+let teamMembers = [
+    ["Dana", "Brenda", "Happy"],
+    ["Laura", "Patti"],
+    ["Leslye", "Randy", "Mollye", "Ranse"],
+    ["Eloise", "Robert"]
+    ];
+    let numTeams = teamMembers.length;
+for (let i = 0; i < numTeams; i++) {
+console.log("-----------");
+console.log("Team " + (i + 1));
+console.log("-----------");
+let numMembers = teamMembers[i].length;
+for (let j = 0; j < numMembers; j++) {
+console.log(teamMembers[i][j]);
+}
+}
